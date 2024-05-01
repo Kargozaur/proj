f95f8e39cca9f982ce3d247829fa357ef6880515
Kargo <kargozaurus@gmail.com>
2024-05-01 21:01:07 +0300

    Created db

---
+++ core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "Databasehelper",
+    "db_helper",
+    "Product",
+)
+
+from .base import Base
+from .product import Product
+from .db_helper import Databasehelper, db_helper
--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from pydantic import BaseModel, EmailStr
 import uvicorn
 from items_view import router as items_router
 from users.views import router as user_router
+from contextlib import asynccontextmanager
+from core.models import Base, db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(user_router)
 
